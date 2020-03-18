fb3ae9546290da3400e4d7853fd127f4888b954b
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-18 18:00:15 -0400

    Added more components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import axios from "axios";
+import Content from "./components/Content";
 
 const App = () => {
-  const [photo, setPhoto] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  });
-
-  return <div className='App'></div>;
+  return <Content />;
 };
 
 export default App;
---
+++ src/components/Content.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Content = () => {
+  const [photo, setPhoto] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        // console.log(error);
+      });
+  }, []);
+
+  return <div className='App'></div>;
+};
+
+export default Content;
